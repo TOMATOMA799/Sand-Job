1e740af3167e24b29e9309e90a2cc4d2398ee584
TOMATOMA799 <thomas.woo799@gmail.com>
2026-08-26 14:22:29 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from "react";
+import { AuthProvider, useAuth } from "./AuthContext";
 import SignInPage from "./SignInPage";
+import DirectMessages from "./Applications/DirectMessages/DirectMessages";
+
+function AppRoutes() {
+  const { user } = useAuth();
+  return user ? <DirectMessages /> : <SignInPage />;
+}
 
 export default function App() {
-  return <SignInPage />;
+  return (
+    <AuthProvider>
+      <AppRoutes />
+    </AuthProvider>
+  );
 }
